c5d91c9d05a9489a78f6628afcd39e52adc7903a
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-06-18 10:12:38 +0600

    update

--- app.py
+++ app.py
@@ -22,11 +22,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 def connect_sheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    
-    creds_json = os.getenv("GOOGLE_CREDENTIALS")  # Match exactly!
+
+    creds_json = os.environ.get("GOOGLE_CREDENTIALS")
+    if creds_json is None:
+        raise Exception("⚠️ GOOGLE_CREDENTIALS not found in Streamlit secrets!")
+
     creds_dict = json.loads(creds_json)
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    
+
     client = gspread.authorize(creds)
     sheet = client.open("AI Voice Sheet").sheet1
     sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet.spreadsheet.id}"
